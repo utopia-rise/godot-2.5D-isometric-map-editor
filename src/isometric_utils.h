265a70c275fe249e56695febfae9042e7f1102a0
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-04-19 15:24:03 +0200

    Add vector2 to vector3 and inverse

--- src/isometric_utils.h
+++ src/isometric_utils.h
@@ -5,9 +5,17 @@
 #include <core/Vector2.hpp>
 
 namespace godot {
+
+#define deg2rad(deg) deg / 180 * Math_PI
+
     class IsometricUtils {
     public:
-        static Vector3 get3DCoordFromScreen(Vector2 pos, int orthZ);
+        static const int TILE_WIDTH; //TODO read with JSON
+        static const int TILE_DEPTH; //TODO read with JSON
+        static const int TILE_HEIGHT; //TODO read with JSON
+        static const int E_Z;
+
+        static Vector3 get3DCoordFromScreen(Vector2 pos, real_t orthZ);
         static Vector2 getScreenCoordFrom3D(Vector3 pos);
     };
 }
