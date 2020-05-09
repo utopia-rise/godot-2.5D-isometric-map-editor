50ce56cc6b7d4c6899c65fc58943217aea4dee08
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-05-09 16:09:53 +0200

    Simplify InputEventMouseButton case in map edition handler

--- demo/addons/IsometricMap/map_edition_handler.gd
+++ demo/addons/IsometricMap/map_edition_handler.gd
@@ -120,8 +120,9 @@ func forward_canvas_gui_input(event: InputEvent) -> bool:
 				else:
 					return false
 	elif event is InputEventMouseButton:
-		if event.is_pressed():
-			if event.button_index == BUTTON_LEFT:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				print("mouse button left pressed")
 				var is_selecting_existing := check_and_select_existing()
 				if !is_selecting_existing:
 					var ortho_pos: Vector3 = IsoServer.get_3d_coord_from_screen(map.get_local_mouse_position(), stair_selector.selected_stair).round()
@@ -150,31 +151,29 @@ func forward_canvas_gui_input(event: InputEvent) -> bool:
 						return false
 				else:
 					return true
-			elif event.button_index == BUTTON_RIGHT:
-				pass
-		else:
-			if event.button_index == BUTTON_LEFT:
-				if drag_action == DragAction.TILING:
-					if selected_positionable != null:
-						if selected_positionable.get_class() == "IsometricPlaceholder":
-							print("finished creating placeholder")
-							var root_node: Node = editor_interface.get_edited_scene_root()
-							var positionable_pos: Vector3 = IsoServer.get_3d_coord_from_screen(selected_positionable.iso_position, stair_selector.selected_stair).round()
-							selected_positionable.position3d = positionable_pos
-							map.remove_child(selected_positionable)
-							undo_redo.create_action("create_placeholder")
-							undo_redo.add_do_method(map, "add_iso_positionable", selected_positionable)
-							undo_redo.add_do_method(selected_positionable, "set_owner", root_node)
-							undo_redo.add_do_method(selected_positionable, "_on_grid_updated", stair_selector.selected_stair)
-							undo_redo.add_undo_method(map, "remove_iso_positionable", selected_positionable)
-							undo_redo.commit_action()
-							drag_action = DragAction.NONE
-							scene_from_tile_tree(false)
-							return true
-						else:
-							drag_action = DragAction.NONE
-			elif event.button_index == BUTTON_RIGHT:
-				pass
+			else:
+				if event.button_index == BUTTON_LEFT:
+					if drag_action == DragAction.TILING:
+						if selected_positionable != null:
+							if selected_positionable.get_class() == "IsometricPlaceholder":
+								print("finished creating placeholder")
+								var root_node: Node = editor_interface.get_edited_scene_root()
+								var positionable_pos: Vector3 = IsoServer.get_3d_coord_from_screen(selected_positionable.iso_position, stair_selector.selected_stair).round()
+								selected_positionable.position3d = positionable_pos
+								map.remove_child(selected_positionable)
+								undo_redo.create_action("create_placeholder")
+								undo_redo.add_do_method(map, "add_iso_positionable", selected_positionable)
+								undo_redo.add_do_method(selected_positionable, "set_owner", root_node)
+								undo_redo.add_do_method(selected_positionable, "_on_grid_updated", stair_selector.selected_stair)
+								undo_redo.add_undo_method(map, "remove_iso_positionable", selected_positionable)
+								undo_redo.commit_action()
+								drag_action = DragAction.NONE
+								scene_from_tile_tree(false)
+								return true
+							else:
+								drag_action = DragAction.NONE
+		elif event.button_index == BUTTON_RIGHT:
+			return false
 	elif event is InputEventKey:
 		event as InputEventKey
 		if(event.is_pressed()):
