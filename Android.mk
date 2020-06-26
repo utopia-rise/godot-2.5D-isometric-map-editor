9e9f2a0abad5f34149cff96fc4588cc1f62c200d
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-06-27 00:24:39 +0200

    Fix android build

--- Android.mk
+++ Android.mk
@@ -28,11 +28,9 @@ src/positionable/IsometricPositionable.cpp \
 src/positionable/IsometricMap.cpp \
 src/positionable/IsometricTile.cpp \
 src/positionable/IsometricPlaceholder.cpp \
-src/positionable/IsometricElement.cpp \
 src/positionable/StaticIsometricElement.cpp \
 src/positionable/DynamicIsometricElement.cpp \
 src/positionable/PlaceholderType.cpp \
-src/positionable/physics/DefaultBody.cpp \
 src/positionable/physics/DefaultStaticBody.cpp \
 src/positionable/physics/DefaultKinematicBody.cpp \
 src/containers/Grid3D.cpp \
